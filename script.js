d20e260be8075675122f8dd935ffbfa208e7c983
Thu Dang <thudang.dtt@gmail.com>
2021-08-13 15:17:47 -0500

    responsive fixes

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function Validate(input) {
   let format = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   let alert = document.getElementById("alert");
   let error = document.getElementById("error");
-  if (input.value.match(format) === "false") {
+  if (input.value.match(format) === null) {
     alert.style.display = "initial";
     error.style.display = "initial";
     return false;
